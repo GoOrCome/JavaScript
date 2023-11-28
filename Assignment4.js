8923cd8ce1160958505d59d02c9d3f70167a6808
GoOrCome <biswajitmahato0568@gmail.com>
2023-11-29 00:49:59 +0530

    js q4 done

---
+++ Assignment4.js
@@ -0,0 +1,12 @@
+let ticketChild = 100;
+let ticketAdult = 150;
+let ticketSeniors = 120;
+
+let numberOfChild = 2;
+let numberOfAdult = 1;
+let numberOfSeniors = 1;
+
+let totalPrice = ticketChild * numberOfChild + ticketAdult * numberOfAdult + ticketSeniors * numberOfSeniors
+
+
+console.log(`The total ticket price is ${totalPrice}`);
