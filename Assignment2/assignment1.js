437e8ff12bf77980364fd7206a1b5f04dbfb11e0
GoOrCome <biswajitmahato0568@gmail.com>
2024-06-22 14:58:38 +0530

    complete assignment1

---
+++ Assignment2/assignment1.js
@@ -0,0 +1,18 @@
+const AllUser=["Ram","Sam","Biswajit","Tatai","Papai","Krishna","Abdul","Sita"];
+
+function CheckUser(user) {
+  if (AllUser.includes(user)) {
+    console.log(`Yes, ${user} is a valid user.`);
+    return true; 
+  } else{
+    console.log(`No, ${user} is not a valid user.`);
+    return true; 
+  }
+  
+}
+
+CheckUser("Ram")
+// OUTPUT: Yes, Ram is a valid user.
+
+CheckUser("HariRam")
+// OUTPUT: No, HariRam is not a valid user.
