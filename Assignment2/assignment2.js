fba8eac29cea6e26349db114e36cd79494013e5b
GoOrCome <biswajitmahato0568@gmail.com>
2024-06-22 17:00:41 +0530

    complete assignment1

---
+++ Assignment2/assignment2.js
@@ -0,0 +1,13 @@
+function calculateTotalCartValue() {
+    let totalValue = 0;
+    for (let index = 0; index < arguments.length; index++) {
+      totalValue += arguments[index];
+    }
+    console.log(`The total cart value is ${totalValue}`);
+    return totalValue;
+  }
+  
+  calculateTotalCartValue(125, 20, 30);
+  
+  // OUTPUT: The total cart value is 175
+  
---
+++ Assignment2/pre.js
@@ -0,0 +1,26 @@
+function calculateTotalCartValue(...total) {
+    // let totalValue = 0;
+    // console.log(total);
+    return total;
+    // for (let i = 0; i < arguments.length; i++) {
+    //   totalValue += arguments[i];
+    // }
+    // console.log(`The total cart value is ${totalValue}`);
+    // return totalValue;
+  }
+//   for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+//   }
+let p = calculateTotalCartValue(125, 20, 30);
+console.log(p);
+function totalvalues(){
+    let a=0;
+    p.forEach(nub=> p+= nub);
+}
+
+// console.log(calculateTotalCartValue(125, 20, 30));
+totalvalues(p);
+  
+  // OUTPUT: The total cart value is 175
+  
